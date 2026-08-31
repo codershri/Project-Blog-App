fa0bf385b62d2c75155a1b6c9d74f464b24d8294
codershri <shrirang.a.sonawane43@gmail.com>
2026-09-01 00:03:35 +0530

    added registration form sign up

--- Blog/settings.py
+++ Blog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "Blog_App.apps.BlogAppConfig",
+    "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
