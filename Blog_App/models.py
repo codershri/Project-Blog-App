4f887aa1cfda2907486cd86e067bca6a0d598c28
codershri <shrirang.a.sonawane43@gmail.com>
2026-08-31 17:23:39 +0530

    Updated bootstrap added model and started working with database

--- Blog_App/models.py
+++ Blog_App/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
