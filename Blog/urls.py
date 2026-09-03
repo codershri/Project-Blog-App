4a9ab656538f787a186b812200815f327a6f3a19
codershri <shrirang.a.sonawane43@gmail.com>
2026-09-04 01:12:45 +0530

    fixed default pic problem and started with signals file

--- Blog/urls.py
+++ Blog/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_view
 from django.contrib.auth import views as auth_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,4 +30,7 @@ urlpatterns = [
     path('Blog_App/', include('Blog_App.urls')),
     path("login/", auth_view.LoginView.as_view(template_name='users/login.html') , name='login'),
     path("logout/", auth_view.LogoutView.as_view(template_name='users/logout.html') , name='logout'),
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
