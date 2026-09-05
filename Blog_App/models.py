4b62f9d32a66ae1a880c1880b989e1d030022d15
codershri <shrirang.a.sonawane43@gmail.com>
2026-09-06 01:04:52 +0530

    fixing blog posting with login must required

--- Blog_App/models.py
+++ Blog_App/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,4 +12,7 @@ class post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk':self.pk})
     
--- Blog_App/views.py
+++ Blog_App/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
 from .models import post
 
 # Create your views here.
@@ -21,7 +22,14 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = post
-    
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
