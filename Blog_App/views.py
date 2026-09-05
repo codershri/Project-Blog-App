61a5db57533cd34b1d99dcab86198eb86215c9ec
codershri <shrirang.a.sonawane43@gmail.com>
2026-09-06 00:31:12 +0530

    individual Post page updated

--- Blog_App/views.py
+++ Blog_App/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import post
 
 # Create your views here.
@@ -10,6 +11,18 @@ def home(request):
 
     }
     return render(request, 'Blog_App/home.html',context)
+
+class PostListView(ListView):
+    model = post
+    template_name = 'Blog_App/home.html' ## <app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = post
+    
+
 
 def about(request):
     context={
