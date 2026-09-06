8ea283632ec6cb758a0bf6b75bbc41c01683333e
codershri <shrirang.a.sonawane43@gmail.com>
2026-09-06 18:16:22 +0530

    updated the funcitionality and started with pagination

--- Blog_App/views.py
+++ Blog_App/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import post
 
 # Create your views here.
@@ -45,6 +45,14 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False            
 
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author :
+            return True
+        return False  
 
 def about(request):
     context={
