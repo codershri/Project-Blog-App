a96cf292465057bd8c6543f3f7542576d7a71361
codershri <shrirang.a.sonawane43@gmail.com>
2026-09-11 00:05:48 +0530

    reset password email function completed

--- Blog/urls.py
+++ Blog/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path("password_reset/", auth_view.PasswordResetView.as_view(template_name='users/password_reset.html') , name='password_reset'),
     path("password_reset/done/", auth_view.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html') , name='password_reset_done'),
     path("password_reset_confirm/<uidb64>/<token>/", auth_view.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html') , name='password_reset_confirm'),
+    path("password_reset_complete/", auth_view.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html') , name='password_reset_complete'),
 
 ] 
 
